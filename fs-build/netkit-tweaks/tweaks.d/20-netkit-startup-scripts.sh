ba91b0a8b1ac28c02147cef7ce1648eb8598f1ff
Markus.Stenberg <markus.stenberg@iki.fi>
2012-07-03 10:29:12 +0300

    Fixed dependency-based booting to work (required in Debian 6.0), and moved netkit-test-phase to /sbin - it doesn't belong in /etc/init.d anymore.

--- fs-build/netkit-tweaks/tweaks.d/20-netkit-startup-scripts.sh
+++ fs-build/netkit-tweaks/tweaks.d/20-netkit-startup-scripts.sh
@@ -3,4 +3,5 @@
 echo "Installing Netkit startup scripts..."
 eval "${SUDO_PFX}chroot ${FS_MOUNT_DIR} update-rc.d netkit-phase1 defaults 00 99${SUDO_SFX}" >/dev/null
 eval "${SUDO_PFX}chroot ${FS_MOUNT_DIR} update-rc.d netkit-phase2 defaults 99 10${SUDO_SFX}" >/dev/null
-
+eval "${SUDO_PFX}chroot ${FS_MOUNT_DIR} update-rc.d netkit-mount-modules-dir start 00 S stop 99 0 6${SUDO_SFX}" >/dev/null
+eval "${SUDO_PFX}chroot ${FS_MOUNT_DIR} update-rc.d netkit-welcome start 01 S${SUDO_SFX}" >/dev/null
