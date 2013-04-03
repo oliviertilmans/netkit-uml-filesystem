0be637716a4ddf5b5bd43e3e00132a4817b2d188
Markus Stenberg <markus.stenberg@iki.fi>
2013-04-03 15:54:36 +0300

    Changed SUDO_PFX to include space - the old hack with trailing space was not very elegant (extra space at start of command will not hurt shell).

--- fs-build/netkit-tweaks/tweaks.d/20-netkit-startup-scripts.sh
+++ fs-build/netkit-tweaks/tweaks.d/20-netkit-startup-scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Installing Netkit startup scripts..."
-eval "${SUDO_PFX}chroot ${FS_MOUNT_DIR} update-rc.d netkit-phase1 defaults 00 99${SUDO_SFX}" >/dev/null
-eval "${SUDO_PFX}chroot ${FS_MOUNT_DIR} update-rc.d netkit-phase2 defaults 99 10${SUDO_SFX}" >/dev/null
-eval "${SUDO_PFX}chroot ${FS_MOUNT_DIR} update-rc.d netkit-mount-modules-dir start 00 S stop 99 0 6${SUDO_SFX}" >/dev/null
-eval "${SUDO_PFX}chroot ${FS_MOUNT_DIR} update-rc.d netkit-welcome start 01 S${SUDO_SFX}" >/dev/null
+eval "${SUDO_PFX} chroot ${FS_MOUNT_DIR} update-rc.d netkit-phase1 defaults 00 99${SUDO_SFX}" >/dev/null
+eval "${SUDO_PFX} chroot ${FS_MOUNT_DIR} update-rc.d netkit-phase2 defaults 99 10${SUDO_SFX}" >/dev/null
+eval "${SUDO_PFX} chroot ${FS_MOUNT_DIR} update-rc.d netkit-mount-modules-dir start 00 S stop 99 0 6${SUDO_SFX}" >/dev/null
+eval "${SUDO_PFX} chroot ${FS_MOUNT_DIR} update-rc.d netkit-welcome start 01 S${SUDO_SFX}" >/dev/null
--- fs-build/netkit-tweaks/tweaks.d/30-disable-services.sh
+++ fs-build/netkit-tweaks/tweaks.d/30-disable-services.sh
@@ -2,6 +2,6 @@
 
 echo "Disabling unneeded services..."
 for SERVICE in $(<disabled-services); do
-   eval "${SUDO_PFX}chroot ${FS_MOUNT_DIR} update-rc.d -f ${SERVICE} remove${SUDO_SFX}" >/dev/null
+   eval "${SUDO_PFX} chroot ${FS_MOUNT_DIR} update-rc.d -f ${SERVICE} remove${SUDO_SFX}" >/dev/null
 done
 
